c92f7e76240c24644bd2db15378ce0c718888fa0
adam-hotait <adam.hotait@student.ecp.fr>
2018-12-19 07:56:11 +0100

    Optimize in 6x10 case

--- pentomino_symetries.py
+++ pentomino_symetries.py
@@ -112,7 +112,12 @@ def main(argv=[]):
     all_shapes = dict()
     SHAPES = dict()
 
-    FREE_PENTOMINOS = ["U", "V", "X", "Z", "N", "P", "T", "W", "F", "Y", "L", "I"]
+    # Dans le cas, il ny'a pas de formes qui peuvent recouvrir à elle seule un bord, on peut prendre une configuration
+    # plus efficace
+    if N == 6 or M == 6:
+        FREE_PENTOMINOS = ["F", "I", "L", "N", "P", "T", "U", "V", "W", "X", "Y", "Z"]
+    else:
+        FREE_PENTOMINOS = ["U", "V", "X", "Z", "N", "P", "T", "W", "F", "Y", "L", "I"]  # formes possibles
 
     for shape_name, shape_matrix in shapes.items():
         shape_set = set()
